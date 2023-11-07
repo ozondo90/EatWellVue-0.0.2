bbded1e7febbf4eb03bd841257b9b077853963d1
ozondo90 <oliverzondo90@gmail.com>
2023-11-07 19:42:16 +0100

    comment de cors from the backend app file

--- EatWellAppExpress/app.js
+++ EatWellAppExpress/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require("dotenv")
 dotenv.config();
-var cors = require("cors")
+// var cors = require("cors")
 var authentication = require("./middlewares/authentification");
 
 
@@ -17,7 +17,7 @@ var connexionsRouter = require("./routes/connexions")
 
 var app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
